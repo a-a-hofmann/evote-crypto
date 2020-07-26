76895617755082ecb9219d3cf0163bc8be43f34d
Alexander Hofmann <alexhofmann@gmail.com>
2020-07-26 16:55:29 +0200

    Remove unused dependencies, clean up lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+
 #[allow(clippy::many_single_char_names)]
 mod math;
 #[allow(clippy::many_single_char_names)]
@@ -6,9 +7,6 @@ pub mod rsa;
 
 #[cfg(test)]
 mod tests {
-    use crate::rsa::RSA;
-    use num_bigint::BigInt;
-
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
