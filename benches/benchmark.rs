a41ffafa83c16b0327fe12c0857286e23edbe33e
tduong992 <tduong992@gmail.com>
2020-11-14 19:01:09 +0100

    Add all benchmarks back in

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -62,7 +62,7 @@ fn prepare_benchmark<'a>(
 
 fn bench_homomorphic_sum(c: &mut Criterion) {
     let (params, _, _, ciphertext0, ciphertext1) = crypto_material();
-    let elements_count = [1000, 5000, 10_000, 50_000, 100_000, 500_000, 1_000_000];
+    let elements_count = [1000, 10_000, 100_000, 1_000_000];
 
     let mut group = prepare_benchmark(
         c,
@@ -101,7 +101,7 @@ fn bench_homomorphic_sum(c: &mut Criterion) {
 
 fn bench_decrypt_many_votes(c: &mut Criterion) {
     let (params, private_key, _, ciphertext0, ciphertext1) = crypto_material();
-    let elements_count = [1000, 5000, 10_000, 50_000, 100_000, 500_000, 1_000_000];
+    let elements_count = [1000, 10_000, 100_000, 1_000_000];
 
     let mut group = prepare_benchmark(c, "Decryption", SamplingMode::Flat, AxisScale::Logarithmic);
 
@@ -134,68 +134,6 @@ fn bench_decrypt_many_votes(c: &mut Criterion) {
     group.finish();
 }
 
-fn bench_votes_decryption(c: &mut Criterion) {
-    let x = BigInt::from_str_radix("c308aea0cd4859a06964f3aef8193705668887e889b259dcb3475cf793c3ede229ebef203716f56d5aa46f8ddf601da5d34468a1e006b61fd412d56dc41ef01e5144d150c62e3d51b6824ed7514d1a36bce7abbea0501a093f2348d6e6bdfebb0dcebc789ca352b9874fd1519deb85e13af2879394e5ac62e252cac530b6b98da77d7b64c56156ea77f22416815f44e90a879e020ed543f63c03323f2e42d3d14e1c01b7e0c1bad4e289f274ee73f253622c671c0a02688f3cf98607236a99d1f83bde87c4a53ed6910d21501c926d8e492406aa42ef6e0559dc49ca1cd41821f80bcea45d52306c4833a2fd0a73606b714b5d20c4fbaa43d1c94c09fa614a", 16).unwrap();
-    let h = BigInt::from_str_radix("61cb62ec3387adbdf2f01c6169f6493f86890c6779f92f375426ea69c7f7e79baef2ad7319441342690e4dbb428634270a7081571717fc8d997f1c4c7c92f84566c53c123092e4ab1e9df18ddbb9e5f98ca386d8b19d6e65c116ad12bfa07506f57d1890d7a08f8fb1fc0f354d4f8cebee9fc81c06502c8fac80e67fa00fffe14ee3b311a81a20217809e56831a1050e3a61724ecf8682625452ebc290d1d4aca22c29380039e6181bc0e2df19b9a8f76bd6e3a0ea5e089b9182840b661efb9b1ce3ca3f39be2025dcbce2d3f2e56a97f637c79bca16da9e4edb6ebb02564794465cf15d09cdea5f24055016e8bf3d9652eea75df5a4d49e62819e7f2da70f6b", 16).unwrap();
-    let p = BigInt::from_str_radix("ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca18217c32905e462e36ce3be39e772c180e86039b2783a2ec07a28fb5c55df06f4c52c9de2bcbf6955817183995497cea956ae515d2261898fa051015728e5a8aacaa68ffffffffffffffff", 16).unwrap();
-    let g = BigInt::from(2);
-    let params = ElGamalParameters { p, g };
-
-    let private_key = ElGamalPrivateKey {
-        x,
-        params: params.clone(),
-    };
-    let public_key = ElGamalPublicKey {
-        h,
-        params: params.clone(),
-    };
-    let n = [1000, 5000, 10_000, 50_000, 100_000, 500_000, 1_000_000];
-
-    let nonce = BigInt::from_str_radix("fa08aea0cd4859a06964f3aef8193705668887e889b259dcb3475cf793c3ede229ebef203716f56d5aa46f8ddf601da5d34468a1e006b61fd412d56dc41ef01e5144d150c62e3d51b6824ed7514d1a36bce7abbea0501a093f2348d6e6bdfebb0dcebc789ca352b9874fd1519deb85e13af2879394e5ac62e252cac530b6b98da77d7b64c56156ea77f22416815f44e90a879e020ed543f63c03323f2e42d3d14e1c01b7e0c1bad4e289f274ee73f253622c", 16).unwrap();
-    let vote0 = BigInt::from(0);
-    let vote1 = BigInt::from(1);
-
-    let ciphertext0 = ElGamal::encrypt(&vote0, &nonce, &public_key);
-    let ciphertext1 = ElGamal::encrypt(&vote1, &nonce, &public_key);
-
-    let mut group = prepare_benchmark(
-        c,
-        "Homomorphic sum and decryption",
-        SamplingMode::Flat,
-        AxisScale::Logarithmic,
-    );
-
-    for count in n.iter() {
-        let ciphers0: Vec<Cipher> = iter::repeat(ciphertext0.clone())
-            .take((count / 2) as usize)
-            .collect::<Vec<_>>();
-        let ciphers1: Vec<Cipher> = iter::repeat(ciphertext1.clone())
-            .take((count / 2) as usize)
-            .collect::<Vec<_>>();
-        let ciphers: Vec<Cipher> = [ciphers0, ciphers1].concat();
-        assert_eq!(ciphers[0], ciphertext0);
-        assert_eq!(ciphers[(count - 1) as usize], ciphertext1);
-        assert_eq!(ciphers.len(), *count as usize);
-
-        println!("Adding up {} votes, with count {}", ciphers.len(), count);
-        group.throughput(Throughput::Elements(*count as u64));
-        group.sample_size(10).bench_with_input(
-            BenchmarkId::from_parameter(count),
-            &ciphers,
-            |b, ciphers| {
-                b.iter(|| {
-                    let sum = ElGamal::add_parallel(ciphers.clone(), &params);
-
-                    let plaintext = ElGamal::decrypt_with_heuristic(&sum, &private_key, *count);
-
-                    assert_eq!(plaintext, BigInt::from(count / 2));
-                })
-            },
-        );
-    }
-    group.finish();
-}
-
 fn bench_voter_registration(c: &mut Criterion) {
     let exponent = BigUint::from_str_radix("10001", 16).unwrap();
     let modulus = BigUint::from_str_radix("e97a728532da5af885d0fbfc2dbe97854eb7765e209ed8c426092c8c9f22a60bf29018a8c8a86f7957e5dba77f95bfcee901fc8d5a633b60d0a6bb7bc7f6bb63edf229872d223d87b133875161c2502c099a2731a6567346e339eb5fa73460516784eaa1c96eb37270744a152c8908a1cc73aab10608123861b8a466abd5f6f230a11d935675659bee231c61f763f13b192181d40aba657da2693b07af818e472919dbfc756fc2eb59f4e6cc077e6cd2621f3688e4af6a4567d2b9cf55261a8b92733decbbd48ea9a1603c7dd9ed89244bfc54e1007bb429392f9577615dad735b89e3b3f0dbd5d30becbc0ceedd4d1071dba4f141f6961f14cbc671c7babbdd", 16).unwrap();
@@ -206,7 +144,7 @@ fn bench_voter_registration(c: &mut Criterion) {
     let result = verify_signature(address.clone(), &signature, &rsa_component);
     assert_eq!(result, true);
 
-    let n = [1000, 5000, 10_000, 50_000, 100_000, 500_000, 1_000_000];
+    let n = [1000, 10_000, 100_000, 1_000_000];
 
     let mut group = prepare_benchmark(
         c,
@@ -262,7 +200,7 @@ fn bench_ballot_proofs(c: &mut Criterion) {
         h,
         params: params.clone(),
     };
-    let n = [1000, 5000, 10_000];
+    let n = [1000, 10_000, 100_000, 1_000_000];
 
     let nonce = BigInt::from_str_radix("7a2999ab10808130a30c33c8e7b023ea91875bbca501fe96922f006634e44d61e418eccda3e1b028e118a685e7d300a432dd529245110d39f982f78b63f3e519d2629fc0ec2e4ffec8e97397b4a3741c26fd26c32908320fc491bd45ee7ddc5221d27b7a5f5518dafad0517d8486a9d76963c31178231399ab5e6dc2b2b06885a8237eefead8686845b29c87d938e1c1ad03acf51873ff91a49568f334f0d8dd68bc77f85a642b3aafa35f860ad110c9906c0c66c68543fbee4d73fc5e0f55f129687548dfd9191a4c9e66d89d1047d70937853a7f95c4f8435b837ad9776af7c32c4b75b54ebe7af498ba97f81edd3270e567199b4426f3707fd86438be4c", 16).unwrap();
     let vote0 = BigInt::from(0);
@@ -338,5 +276,5 @@ fn bench_ballot_proofs(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, bench_voter_registration);
+criterion_group!(benches, bench_homomorphic_sum,  bench_voter_registration, bench_decrypt_many_votes, bench_ballot_proofs);
 criterion_main!(benches);
